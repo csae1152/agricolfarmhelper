3608562cf023e22a0c1cb9957c72b8618891c763
Helmut Steiner <helmut.steinersti2@gmail.com>
2020-05-23 11:10:42 +0200

    Initial

---
+++ src/main/java/com/agricol/farmdiagnosis/FarmdiagnosisApplication.java
@@ -0,0 +1,15 @@
+package com.agricol.farmdiagnosis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class FarmdiagnosisApplication {
+
+	public static void main(String[] args) {
+		run(FarmdiagnosisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agricol/farmdiagnosis/FarmdiagnosisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agricol.farmdiagnosis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmdiagnosisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
